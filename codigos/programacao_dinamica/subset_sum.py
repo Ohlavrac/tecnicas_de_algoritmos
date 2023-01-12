25f0f7a6805fec7da1b0ac9761d747249b4a5ac4
Ohlavrac <joaovicrcarvalho@gmail.com>
2023-01-11 22:07:45 -0300

    fix: subsetsum

--- codigos/programacao_dinamica/subset_sum.py
+++ codigos/programacao_dinamica/subset_sum.py
@@ -1,5 +1,10 @@
-def isSubSetSum(number, set, sum):
-    subset = ([False for index in range(sum + 1)] for index in range(number + 1))
+#problema da soma de subconjuntos
+def isSubSetSum(set, number, sum):
+    
+    subset =([[False for i in range(sum + 1)] 
+            for i in range(number + 1)])
+    
+    print(subset[0])
     
     for index in range(number + 1):
         subset[index][0] = True
@@ -14,12 +19,13 @@ def isSubSetSum(number, set, sum):
             if index2 >= set[index - 1]:
                 subset[index][index2] = (subset[index - 1][index2] or subset[index - 1][index2 - set[index - 1]])
     
+    print(f'>>>> {subset}')
     return subset[number][sum]
 
 set = [3, 34, 4, 12, 5, 2]
 sum = 9
-n = len(set)
-if (isSubSetSum(set, n, sum) == True):
+number = len(set)
+if (isSubSetSum(set, number, sum) == True):
     print("Encontrado o subset com a determinada soma")
 else:
     print("Subset não encontrado")
