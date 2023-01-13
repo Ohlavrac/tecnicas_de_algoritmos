5d6ab0af04f6d09f6c60519336657c8c944a9e80
DeboraCrist <deboracbrito@aluno.uespi.br>
2023-01-12 21:25:47 -0300

    alterando variavel

--- codigos/recursividade/maximo_recursivo.py
+++ codigos/recursividade/maximo_recursivo.py
@@ -1,16 +1,16 @@
-def max_recursive(lista, numero):
-    if numero == 1:
+def max_recursive(lista, tamanho):
+    if tamanho == 1:
         return lista[0]
     else:
         x = int()
-        x = max_recursive(lista=lista, numero=numero - 1)
-        if x > lista[numero - 1]:
+        x = max_recursive(lista=lista, tamanho=tamanho - 1)
+        if x > lista[tamanho - 1]:
             return x
         else:
-            return lista[numero - 1]
+            return lista[tamanho - 1]
 
 
 l = [2, 5, 3, 40, 29, 49, 23, 1]
-maxi = max_recursive(lista=l, numero=len(l))
+maxi = max_recursive(lista=l, tamanho=len(l))
 print(l)
 print(f'O maior número do array é: {maxi}')
