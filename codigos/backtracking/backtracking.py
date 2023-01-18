3c784d9bb4cf725d067359a298da4b70b7ee5459
DeboraCrist <deboracbrito@aluno.uespi.br>
2023-01-18 14:09:26 -0300

    codigo do backtracking

--- codigos/backtracking/backtracking.py
+++ codigos/backtracking/backtracking.py
@@ -1,1 +1,17 @@
-#need a code here 
+def subconjuntosUtil(A, subconjuto, index):
+	print(*subconjuto)
+	for i in range(index, len(A)):
+		subconjuto.append(A[i])
+		subconjuntosUtil(A, subconjuto, i + 1)
+		subconjuto.pop(-1)
+	return
+
+def subconjuntos(A):
+	global res
+	subconjunto = []
+	index = 0
+	subconjuntosUtil(A, subconjunto, index)
+	
+valores = [1, 2, 3]
+
+subconjuntos(valores)
